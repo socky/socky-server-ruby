828df3d0b0ffb723d87683f4872c1ae53520bc5d
Bernard Potocki <bernard.potocki@imanel.org>
2011-03-08 22:14:04 +0100

    Find and create new channels

--- lib/socky/channel.rb
+++ lib/socky/channel.rb
@@ -5,5 +5,17 @@ module Socky
     autoload :Base,     "#{ROOT}/channel/base"
     autoload :Private,  "#{ROOT}/channel/private"
     autoload :Public,   "#{ROOT}/channel/public"
+    
+    # Find or create by channel by name
+    # @param [String] name name of channel
+    # @return [Channel::Base] channel instance
+    def self.find_or_create(name)
+      channel_type = case name.match(/\A\w+/).to_s
+        when 'private' then Private
+        else Public
+      end
+      
+      channel_type.find_or_create(name)
+    end
   end
 end
--- lib/socky/channel/base.rb
+++ lib/socky/channel/base.rb
@@ -1,6 +1,28 @@
 module Socky
   class Channel
     class Base
+      
+      attr_accessor :name
+      
+      class << self
+        # List of all already registered channels of current type
+        def list
+          @list ||= {}
+        end
+        
+        # Find channel or create new
+        # @param [String] name name for channel
+        # @return [Base] channel instance
+        def find_or_create(name)
+          list[name] ||= self.new(name)
+        end
+      end
+      
+      # Initialize new channel
+      # @param [String] name name for channel
+      def initialize(name)
+        @name = name
+      end
 
     end
   end
